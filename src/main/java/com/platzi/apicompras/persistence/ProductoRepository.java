9747a3428a3a1af7772a245031703e9d1208a4ae
carhernandez <carlos.hernandez@convertic.com.co>
2022-12-28 12:39:46 -0500

    actua

--- src/main/java/com/platzi/apicompras/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/apicompras/persistence/ProductoRepository.java
@@ -1,19 +1,40 @@
 package com.platzi.apicompras.persistence;
 
+import com.platzi.apicompras.domain.Product;
+import com.platzi.apicompras.domain.repository.ProductRepository;
 import com.platzi.apicompras.persistence.crud.ProductoCrudRepository;
 import com.platzi.apicompras.persistence.entity.Producto;
+import com.platzi.apicompras.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
 
     private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
+    @Override
+    public List<Product> getAll() {
+        List<Producto> productos =(List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
 
-    public List<Producto> getAll() {
-        return (List<Producto>) productoCrudRepository.findAll();
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.finByidCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return null;
     }
 
     private List<Producto> getByCategoria(int idCategoria) {
